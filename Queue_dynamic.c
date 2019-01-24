7a5a9494bc253611a98113a5709a2b927dc6c8a8
VIVEK KUMAR SAHU <vksahu399@gmail.com>
2019-01-24 19:52:58 +0530

    Update

--- Queue_dynamic.c
+++ Queue_dynamic.c
@@ -1,15 +1,15 @@
 # include<stdio.h>
 # include<stdlib.h>
-struct node
+struct Queue
 {
     int data;
-    struct node* front;
-    struct node* rear;
+    struct Queue* next;
 };
-struct node* root= NULL;
+struct Queue* front= NULL;
+struct Queue* rear=NULL;
 int len;
-void append();
-void Delete();
+void enQueue();
+void DeQueue();
 int lenth();
 void display();
 int main()
@@ -18,8 +18,8 @@ int main()
     while(1)
     {
         printf("\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*MENU*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n");
-        printf("1. Append :\n");
-        printf("2. Delete :\n");
+        printf("1. EnQueue :\n");
+        printf("2. DeQueue :\n");
         printf("3. Lenth :\n");
         printf("4. Display :\n");
         printf("5. Quite : \n");
@@ -28,9 +28,9 @@ int main()
         scanf("%d",&ch);
         switch(ch)          
         {
-            case 1: append();
+            case 1: enQueue();
                     break;
-            case 2: Delete();
+            case 2: DeQueue();
                     break;
             case 3: len=lenth();
                     printf("\nLenth of linklist is -->: %d \n",len);
@@ -40,5 +40,61 @@ int main()
             case 5: exit(0); 
             default:printf("\nInvalid input\n");
         }
+    }
+}
+
+void enQueue()
+{
+    int ele;
+    printf("Enter the Element :");
+    scanf("%d",&ele);
+    struct Queue* temp=(struct Queue*)malloc(sizeof(struct Queue));
+    temp->data=ele;
+    temp->next=NULL;
+    if(rear==NULL)
+    {
+        front=temp;
+        rear=temp;
+    }else
+    {
+        rear->next=temp;
+        rear=temp;
+    }
+}
+
+int lenth()
+{
+    int count=0;
+    struct Queue* temp=front;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        count++;
+    }
+    return count;
+}
+
+void display()
+{
+    struct Queue* temp=front;
+    printf("Elements is/are :");
+    while(temp!=NULL)
+    {
+        printf("%d-->",temp->data);
+        temp=temp->next;
+    }    
+}
+
+void DeQueue()
+{
+    struct Queue* temp;
+    if(front==NULL)
+    {
+        printf("UNDERFLOW!!!");
+    }else
+    {
+        temp=front;
+        front=front->next;
+        free(temp);
     }
 }
